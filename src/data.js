932d37fd3fb6856f4e141b0b9bc3141c43fadfd2
Jonathan Lee <jlee1546.jl@gmail.com>
2022-10-09 11:37:56 -0400

    Change country value to all caps, add id key to each data entry

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: 1,
+    imgLink: "https://source.unsplash.com/WLxQvbMyfas",
+    country: "JAPAN",
+    destination: "Mount Fuji",
+    date: "12 Jan, 2021 - 24 Jan, 2021",
+    note: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+  },
+  {
+    id: 2,
+    imgLink: "https://source.unsplash.com/JmuyB_LibRo",
+    country: "AUSTRALIA",
+    destination: "Sydney Opera House",
+    date: "27 May, 2021 - 8 Jun, 2021",
+    note: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+  },
+  {
+    id: 3,
+    imgLink: "https://source.unsplash.com/3PeSjpLVtLg",
+    country: "NORWAY",
+    destination: "Geirangerfjord",
+    date: "01 Oct, 2021 - 18 Nov, 2021",
+    note: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+  },
+];
+
+export default data;
