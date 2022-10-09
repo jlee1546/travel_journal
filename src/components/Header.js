a36083b9441fffc51474b2d85ee7f3aca0990603
Jonathan Lee <jlee1546.jl@gmail.com>
2022-10-09 11:36:52 -0400

    Add header--text class

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <i className="fa-solid fa-earth-americas globe"></i>
+      <div className="header--text">my travel journal</div>
+    </header>
+  );
+}
+
+export default Header;
